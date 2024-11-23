1a59c67b24029b9bb413c5905c1434fe5ea8b415
aaahyunseo <hs030608@naver.com>
2024-11-24 01:31:16 +0900

    feat: QuestionUpload.js 꼬리 질문 판단 로직 추가

--- src/pages/QuestionUpload.js
+++ src/pages/QuestionUpload.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './QuestionUpload.css';
+import React, { useState } from "react";
+import "./QuestionUpload.css";
 
-function QuestionUpload({ selectedJob, onFileUpload }) {
+function QuestionUpload({ selectedJob, questionType = "질문", questionContent, onFileUpload }) {
   const [file, setFile] = useState(null);
 
   const handleFileChange = (event) => {
@@ -18,17 +18,15 @@ function QuestionUpload({ selectedJob, onFileUpload }) {
   return (
     <div className="question-upload">
       <h2>{selectedJob} 직군 질문</h2>
-      <p>질문: "{selectedJob} 관련된 질문 예시입니다."</p>
+      <p>
+        {questionType}: "{questionContent}"
+      </p>
       <label className="file-upload-label" htmlFor="file-upload">
-        <br></br>
-        {file ?  "🔊 답변 파일이 업로드 되었습니다 : "+file.name : "🔊 답변 파일을 업로드 해주세요."}
+        {file
+          ? `🔊 답변 파일이 업로드 되었습니다: ${file.name}`
+          : "🔊 답변 파일을 업로드 해주세요."}
       </label>
-      <input 
-        id="file-upload" 
-        type="file" 
-        onChange={handleFileChange} 
-      />
-      
+      <input id="file-upload" type="file" onChange={handleFileChange} />
       <button onClick={handleUpload} disabled={!file}>
         업로드
       </button>
